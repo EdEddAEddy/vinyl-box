2be09ec27d743ed7624201bfee5d8597e2f1e8ac
Edevando Alves <edevando.xavier@gmail.com>
2024-09-14 05:51:49 -0300

    [FEAT] middleware authentication

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { userRegister, userLogin } from "../controllers/users.js";
 import { schemaCreateUser, schemaUserLogin } from "../schemas/userSchema.js";
+import { tokenVerify } from "../middleware/authentication.js";
 import validateReqBody from "../middleware/validatedBody.js";
 
 const router = express.Router();
 
 router.post("/register", validateReqBody(schemaCreateUser), userRegister);
 router.post("/login", validateReqBody(schemaUserLogin), userLogin);
+
+router.use(tokenVerify);
+
+router.get("/home");
 
 export default router;
